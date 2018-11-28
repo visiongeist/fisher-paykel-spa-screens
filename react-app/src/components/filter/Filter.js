087f08ca8f1e6c6a8bb11fe1f4c22859b1a96bf5
Chadwick Cheng <chcheng@192-168-1-190.tpgi.com.au>
2018-11-28 12:41:02 +1100

    Fixed a bug when there is no options in filter

--- react-app/src/components/filter/Filter.js
+++ react-app/src/components/filter/Filter.js
@@ -9,7 +9,7 @@ const FilterEditConfig = {
     emptyLabel: 'Filter',
 
     isEmpty: function(props) {
-        return !props || !props.items || props.items.length < 1;
+        return !props || !props.values || props.values.length < 1;
     }
 };
 
@@ -17,13 +17,13 @@ const FilterEditConfig = {
 /**
  * Renders the filter type and options from spa-screens/components/content/filter
  */
-export default class Filter extends Component {
+export default class Filter extends Component {	
     render() {
         return (
                 <div className="Filter">
                     <p>Filter by {this.props.filterBy} with the below:</p>
                     <ul>
-                      {this.props.values.map(value => <li>{value}</li>)}
+                      {this.props.values ? this.props.values.map(value => <li>{value}</li>) : <li>Empty Options</li>}
                     </ul>  
                 </div>
         );
