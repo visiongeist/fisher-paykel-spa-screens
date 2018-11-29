95291ef24155801971cc869833f8303f70dd5127
Chadwick Cheng <chcheng@chadwicks-mbp.pac.adobe.com>
2018-11-29 15:54:36 +1100

    Displaying HWD

--- react-app/src/components/list/List.js
+++ react-app/src/components/list/List.js
@@ -34,6 +34,7 @@ class ListItem extends Component {
             <li className="ListItem">
                 <Link className="ListItem-link" to={this.props.url}>{this.props.title}
                     <span className="ListItem-date">{this.date}</span>
+                    {this.props.height ? <span className="ListItem-dimension">H {this.props.height} x W {this.props.width} x D {this.props.depth}</span> : ""} 
                 </Link>
             </li>
         );
@@ -45,14 +46,26 @@ class ListItem extends Component {
  */
 export default class List extends Component {
     render() {
+    	let view = this.props.renderType;
+    	var results;
+    	if(view == "ProductView"){
+    		results = this.props.items.map((listItem, index) => {
+                return <ListItem height={listItem.product.height} width={listItem.product.width}
+				 depth={listItem.product.depth}
+				 key={listItem.path} path={listItem.path} url={listItem.url} 
+                title={listItem.title} date={listItem.lastModified} />
+});
+    	} else {
+    		results = this.props.items.map((listItem, index) => {
+                return <ListItem key={listItem.path} path={listItem.path} url={listItem.url} 
+                title={listItem.title} date={listItem.lastModified} />
+});
+    	}
+    	
         return (
                 <div className="List">
                     <ul className="List-wrapper">
-                        { this.props.items && this.props.items.map((listItem, index) => {
-                            return <ListItem key={listItem.path} path={listItem.path} url={listItem.url} 
-                                             title={listItem.title} date={listItem.lastModified} />
-                            })
-                       }
+                        { results}
                     </ul>
                 </div>
         );
