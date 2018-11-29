92f1bf696c13443f5b586cfabedb2da32586d182
Akram Darwazeh <akramd@datacom.co.nz>
2018-11-30 09:37:09 +1300

    Complete basic carousel structure

--- react-app/src/components/carousel/Carousel.jsx
+++ react-app/src/components/carousel/Carousel.jsx
@@ -10,7 +10,7 @@ class Carousel extends Component {
             infinite: true,
             speed: 500,
             slidesToShow: 4,
-            slidesToScroll: 4,
+            slidesToScroll: 2,
             arrows: true,
             className: "carousel",
             // accessibility: true
@@ -25,7 +25,15 @@ class Carousel extends Component {
                 </div>
                 <div className="carousel-container">
                     <Slider {...settings}>
-                        <CarouselCard url="/mmhmm" title="This weird thing" image="/fridges/fridge-373.jpg" />
+                        <CarouselCard url="/mmhmm" title="＼（＾▽＾）／" image="/fridges/fridge-373.jpg" description="Contemporary" />
+                        <CarouselCard url="/mmhmm" title="ヾ(＠°▽°＠)ﾉ" image="/fridges/fridge-373.jpg" description="I'm obviously exploring my kanjmojies" />
+                        <CarouselCard url="/mmhmm" title="|ω･)و ̑̑༉" image="/fridges/fridge-373.jpg" description="Carousel thingy" />
+                        <CarouselCard url="/mmhmm" title="(=ｘェｘ=)" image="/fridges/fridge-373.jpg" description="This weird thing" />
+                        <CarouselCard url="/mmhmm" title="へ（>_<へ)" image="/fridges/fridge-373.jpg" description="This weird thing" />
+                        <CarouselCard url="/mmhmm" title="✖‿✖" image="/fridges/fridge-373.jpg" description="This weird thing" />
+                        <CarouselCard url="/mmhmm" title="(＊0＊;)" image="/fridges/fridge-373.jpg" description="This weird thing" />
+                        <CarouselCard url="/mmhmm" title="ヘ(゜Д、゜)ノ" image="/fridges/fridge-373.jpg" description="This weird thing" />
+
                     </Slider>
                 </div>
             </div>
--- react-app/src/components/carousel/components/CarouselCard.jsx
+++ react-app/src/components/carousel/components/CarouselCard.jsx
@@ -7,14 +7,16 @@ import './CarouselCard.scss';
  *  - url
  *  - path
  *  - image
+ *  - description
  */
 export default class CarouselCard extends Component {
     render() {
         return (
             <Link className="CategoryItem" to={this.props.url}>
                 <div className="carousel-card">
-                    <h3>{this.props.title}</h3>
                     <img src={this.props.image} alt={this.props.title} />
+                    <h4>{this.props.title}</h4>
+                    <h5>{this.props.description}</h5>
                 </div>
             </Link>
 
