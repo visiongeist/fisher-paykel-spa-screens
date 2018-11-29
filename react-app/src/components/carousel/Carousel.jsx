ae10c7baac0464938ed7e83fc658d0f063a517aa
Akram Darwazeh <akramd@datacom.co.nz>
2018-11-30 09:07:50 +1300

    Automatically resize height depending on orientation

--- react-app/src/components/carousel/Carousel.jsx
+++ react-app/src/components/carousel/Carousel.jsx
@@ -1,9 +1,9 @@
 import Slider from 'react-slick';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Carousel.scss';
 import CarouselCard from './components/CarouselCard';
 
-class Carousel extends Component{
+class Carousel extends Component {
     render() {
         const settings = {
             dots: true,
@@ -23,9 +23,9 @@ class Carousel extends Component{
                         {this.props.categoryName}
                     </h2>
                 </div>
-                <div className="carousel-container">     
+                <div className="carousel-container">
                     <Slider {...settings}>
-                    <CarouselCard url="/mmhmm" title="This weird thing" image="/fridges/fridge-373.jpg"/>
+                        <CarouselCard url="/mmhmm" title="This weird thing" image="/fridges/fridge-373.jpg" />
                     </Slider>
                 </div>
             </div>
