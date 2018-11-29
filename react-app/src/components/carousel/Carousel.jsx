39d08f58f3caead67c1ce82d7c1527daa7acf664
Akram Darwazeh <akramd@datacom.co.nz>
2018-11-30 10:32:28 +1300

    WIP: test for touch screen

--- react-app/src/components/carousel/Carousel.jsx
+++ react-app/src/components/carousel/Carousel.jsx
@@ -10,7 +10,7 @@ class Carousel extends Component {
             infinite: true,
             speed: 500,
             slidesToShow: 4,
-            slidesToScroll: 2,
+            slidesToScroll: 4,
             arrows: true,
             className: "carousel",
             // accessibility: true
@@ -25,18 +25,16 @@ class Carousel extends Component {
                 </div>
                 <div className="carousel-container">
                     <Slider {...settings}>
-                        <CarouselCard url="/mmhmm" title="＼（＾▽＾）／" image="/fridges/fridge-373.jpg" description="Contemporary" />
-                        <CarouselCard url="/mmhmm" title="ヾ(＠°▽°＠)ﾉ" image="/fridges/fridge-373.jpg" description="I'm obviously exploring my kanjmojies" />
-                        <CarouselCard url="/mmhmm" title="|ω･)و ̑̑༉" image="/fridges/fridge-373.jpg" description="Carousel thingy" />
-                        <CarouselCard url="/mmhmm" title="(=ｘェｘ=)" image="/fridges/fridge-373.jpg" description="This weird thing" />
-                        <CarouselCard url="/mmhmm" title="へ（>_<へ)" image="/fridges/fridge-373.jpg" description="This weird thing" />
-                        <CarouselCard url="/mmhmm" title="✖‿✖" image="/fridges/fridge-373.jpg" description="This weird thing" />
-                        <CarouselCard url="/mmhmm" title="(＊0＊;)" image="/fridges/fridge-373.jpg" description="This weird thing" />
-                        <CarouselCard url="/mmhmm" title="ヘ(゜Д、゜)ノ" image="/fridges/fridge-373.jpg" description="This weird thing" />
+                        <CarouselCard url="/1" title="＼（＾▽＾）／" image="/fridges/fridge-373.jpg" description="Contemporary" />
+                        <CarouselCard url="/2" title="ヾ(＠°▽°＠)ﾉ" image="/fridges/fridge-373.jpg" description="I'm obviously exploring my kanjmojies" />
+                        <CarouselCard url="/3" title="|ω･)و ̑̑༉" image="/fridges/fridge-373.jpg" description="Carousel thingy" />
+                        <CarouselCard url="/4" title="(=ｘェｘ=)" image="/fridges/fridge-373.jpg" description="This weird thing" />
+                        <CarouselCard url="/5" title="へ（>_<へ)" image="/fridges/fridge-373.jpg" description="This weird thing" />
+                        <CarouselCard url="/6" title="✖‿✖" image="/fridges/fridge-373.jpg" description="This weird thing" />
+                        <CarouselCard url="/7" title="(＊0＊;)" image="/fridges/fridge-373.jpg" description="This weird thing" />
+                        <CarouselCard url="/8" title="ヘ(゜Д、゜)ノ" image="/fridges/fridge-373.jpg" description="This weird thing" />
 
                     </Slider>
-                </div>
-                <div className="footer">
                 </div>
             </div>
         );
