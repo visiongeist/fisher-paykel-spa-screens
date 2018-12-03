5d879f08f7d4ab3e7deb98ce999e23d21cbd8dce
Akram Darwazeh <akramd@datacom.co.nz>
2018-12-04 08:49:07 +1300

    Create footer
    Create and style the footer for portrait view
    
    wish me luck for landscape

--- react-app/src/components/footer/Footer.jsx
+++ react-app/src/components/footer/Footer.jsx
@@ -1,28 +1,31 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Footer.scss';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-export default class Footer extends Component{
+export default class Footer extends Component {
 
-    getClasses(categoryItem){
-        let selected = categoryItem === this.props.selectedCategory ? ' footer-item--active': ''; 
+    getClasses(categoryItem) {
+        let selected = categoryItem.title === this.props.selectedCategory ? ' footer-item--active' : '';
+        console.log(categoryItem);
         return `footer-item${selected}`
     }
 
     render() {
         return (
             <div className="footer">
-               { this.props.categories && this.props.categories.map((categoryItem, index) => {
-                    let c = this.getClasses(categoryItem);
-                    return (
-                        <div key={index} className={c}>
-                            <Link key={categoryItem.path} to={categoryItem.url}>
-                                <div className="textSegment">{categoryItem.title}</div>
-                            </Link>
-                        </div>
-                    );
+                <ul>
+                    {this.props.categories && this.props.categories.map((categoryItem, index) => {
+                        let c = this.getClasses(categoryItem);
+                        return (
+                            <li key={index} className={c}>
+                                <Link key={categoryItem.path} to={categoryItem.url}>
+                                    <div className="textSegment">{categoryItem.title}</div>
+                                </Link>
+                            </li>
+                        );
                     })}
+                </ul>
             </div>
-        );  
+        );
     }
 }
