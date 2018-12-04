50b7cd7a7dfb7cebdb4c2005e9f6675d9110f066
Akram Darwazeh <akramd@datacom.co.nz>
2018-12-04 13:26:44 +1300

    Change back to class component
    if any thing needs to be a class component it's this one.

--- react-app/src/components/productPage/ProductPage.jsx
+++ react-app/src/components/productPage/ProductPage.jsx
@@ -1,30 +1,35 @@
 import React, { Component } from 'react';
 import './ProductPage.scss';
 
-var ProductPage = (props) => {
-    return (
-        <div className="product-page">
-            <div className="information">
-                <div className="model-number">
-                    <h5>
-                    </h5>
+export default class ProductPage extends Component {
+    render() {
+        return (
+            <div className="product-page">
+                <div className="information">
+                    <div className="model-number">
+                        <h5>
+                            what
+                        </h5>
+                    </div>
+                    <div className="title">
+                        <h1>
+                            who
+                        </h1>
+                    </div>
+                    <div className="category">
+                        <h5>
+                            when
+                        </h5>
+                    </div>
+                    <hr />
+                    <div className="description">
+                        <strong>
+                            why
+                        </strong>
+                    </div>
                 </div>
-                <div className="title">
-                    <h1>
-                    </h1>
-                </div>
-                <div className="category">
-                    <h5>
-                    </h5>
-                </div>
-                <div className="description">
-                    <strong>
-                    </strong>
-                </div>
+                <div className="img-container"><img src="fridges/fridge-534L.png" alt="none yet"/></div>
             </div>
-            <div className="img-container"><img src="/unknown.png" alt="no image yet"/></div>
-        </div>
-    );
+        );
+    }
 }
-
-export default ProductPage;
