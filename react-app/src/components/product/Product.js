f0fabeb61a0d9a643faf04b0295408e97dba62da
Akram Darwazeh <akramd@datacom.co.nz>
2018-12-05 11:21:41 +1300

    Move rendering of current view up to state of Product.js

--- react-app/src/components/product/Product.js
+++ react-app/src/components/product/Product.js
@@ -7,7 +7,6 @@
 import React, { Component } from 'react';
 import { MapTo } from '@adobe/cq-react-editable-components';
 import Footer from '../footer/Footer';
-import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 require('./Product.scss');
 /**
@@ -59,26 +58,60 @@ const ProductComponent = (props) => {
  * Product React component
  */
 export default class Product extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentView: <ProductComponent image={this.props.image}
+                description={this.props.features[0].description}
+                title={this.props.title}
+                sku={this.props.sku}
+                category={this.props.category} />,
+            selectedCategory: 'product'
+        };
+    }
+
+    /**
+     * Show the product view
+     */
+    showProduct() {
+        this.setState({
+            currentView: <ProductComponent image={this.props.image}
+                description={this.props.features[0].description}
+                title={this.props.title}
+                sku={this.props.sku}
+                category={this.props.category} />,
+            selectedCategory: 'product'
+        });
+    }
+
+    showFeatures() {
+        this.setState({selectedCategory: 'features'})
+    }
+
+    showSpecs() {
+        this.setState({selectedCategory: 'specs'})
+    }
+
     createFooter() {
         let index = 0;
         let footer = [
-            (<li key={index++} title="product">
-                <Link to='google'>
+            (<li key={index++} title="product" onClick={() => this.showProduct()}>
+                <div>
                     <FontAwesomeIcon icon={['fa', 'info-circle']} />
                     <div className="textSegment">product</div>
-                </Link>
+                </div>
             </li>),
-            (<li key={index++} title="features">
-                <Link to='google'>
+            (<li key={index++} title="features" onClick={() => this.showFeatures()}>
+                <div>
                     <FontAwesomeIcon icon={['fa', 'clone']} />
                     <div className="textSegment">features</div>
-                </Link>
+                </div>
             </li>),
-            <li key={index++} title="specs">
-                <Link to='google'>
+            <li key={index++} title="specs" onClick={() => this.showSpecs()}>
+                <div>
                     <FontAwesomeIcon icon={['fa', 'ruler-horizontal']} />
                     <div className="textSegment">specs</div>
-                </Link>
+                </div>
             </li>
         ];
         return footer;
@@ -86,13 +119,9 @@ export default class Product extends Component {
 
     render() {
         return (<div className="product-page">
-            <ProductComponent image={this.props.image}
-                description={this.props.features[0].description}
-                title={this.props.title} 
-                sku={this.props.sku}
-                category={this.props.category}/>
+            {this.state.currentView}
             <div className="bottom">
-                <Footer selectedCategory="product" >
+                <Footer selectedCategory={this.state.selectedCategory} >
                     {this.createFooter()}
                 </Footer>
             </div>
