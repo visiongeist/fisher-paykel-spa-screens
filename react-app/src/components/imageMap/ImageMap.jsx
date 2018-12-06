6d632341d8986bb59ac0ba842b0ce70038e1e1d7
Akram Darwazeh <akramd@datacom.co.nz>
2018-12-06 16:32:38 +1300

    Add ImageMapper react component

--- react-app/src/components/imageMap/ImageMap.jsx
+++ react-app/src/components/imageMap/ImageMap.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ImageMapper from 'react-image-mapper';
 import './ImageMap.scss';
 /**
  * Image map component which swaps images depending on where it is clicked.
@@ -9,8 +10,6 @@ export default class ImageMap extends Component {
         super(props);
         this.startImage = this.props.image;
         this.state = {
-            image: this.startImage,
-            mappings: this.props.hotSpots,
         };
     }
 
@@ -18,14 +17,14 @@ export default class ImageMap extends Component {
      * Change the image to the one referenced by the link and add the new area mappings
      */
     changeImage(destinationPath) {
-        this.setState({image: destinationPath});
+        this.setState({ image: destinationPath });
     }
 
     /**
      * Go back to the initial landing state, for when the opened image is clicked.
      */
-    loadFirstImage(){
-        if (this.state.image !== this.startImage){
+    loadFirstImage() {
+        if (this.state.image !== this.startImage) {
             this.setState({
                 image: this.startImage,
                 mappings: this.props.hotSpots,
@@ -33,15 +32,28 @@ export default class ImageMap extends Component {
         }
     }
 
+    componentDidMount() {
+        const height = this.divElement.clientHeight;
+        const width = this.divElement.clientWidth;
+        this.setState({
+            imageHeight: height,
+            imageWidth: width
+        });
+    }
+
+    componentDidUpdate() {
+    }
+
     render() {
         return (
-            <div className="click-map" onClick={() => this.loadFirstImage()}>
-                <img src={this.state.image} alt="map" useMap="#image-map" />
+            <div ref={(divElement) => this.divElement = divElement} className="click-map" onClick={() => this.loadFirstImage()}>
+                {/* <img src={this.state.image} alt="map" useMap="#image-map" />
                 <map name="image-map">
                     {this.state.mappings && this.state.mappings.map((area, index) => {
                         return <area key={index} shape={area.shape} coords={area.coordinates} alt={index} href="#" onClick={() => this.changeImage(area.destinationPath)} />
                     })}
-                </map>
+                </map> */}
+                <ImageMapper src={this.props.image} width={this.state.imageWidth} height={this.state.imageHeight} />
             </div>
         );
     }
